06147c8c72ad4c012de42b6fe250a7fc18ebe570
Nelson Oliva <nelson.ricardo02@gmail.com>
2025-12-18 20:03:39 -0500

    add TextNode and TextType classes

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import TextNode, TextType
+
+def main():
+    new_textNode = TextNode("Bold ahh text", TextType.BOLD)
+    print(new_textNode)
+
+if __name__ == "__main__":
+    main()
