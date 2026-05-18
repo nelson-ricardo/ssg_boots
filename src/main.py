df1d40cd8b2a247f6ab6ba4084ae2fcb639d44a4
Nelson Oliva <nelson.ricardo02@gmail.com>
2026-05-18 19:25:48 -0400

    test inline element classes

--- src/main.py
+++ src/main.py
@@ -1,1 +1,14 @@
-print("Hello World!")
+from textnode import TextNode, TextType
+
+def main():
+    regNode = TextNode("this is some anchor text", TextType.IMAGE, "https://www.boot.dev")
+    trueComp = TextNode("this is some anchor text", TextType.IMAGE, "https://www.boot.dev")
+    falseComp = TextNode("gibberish", TextType.BOLD)
+
+    print("Test representation output")
+    print(regNode)
+    print(f"Test if equality works w/ the same object: {regNode == trueComp}")
+    print(f"Test if equality works w/ not the same object: {regNode == falseComp}")
+
+if __name__ == "__main__":
+    main()
