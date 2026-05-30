5c6ab58f293690e4adaa2f9f32ded42bc318e4e2
Nelson Oliva <nelson.ricardo02@gmail.com>
2026-05-30 16:59:40 -0400

    add logic to transform textnode to leafnode

--- src/textnode.py
+++ src/textnode.py
@@ -1,6 +1,8 @@
 from enum import Enum
+from htmlnode import LeafNode
 
 class TextType(Enum):
+    TEXT = "text",
     BOLD = "bold"
     ITALIC = "italic"
     CODE = "code"
@@ -9,7 +11,7 @@ class TextType(Enum):
 
 # TextNode, instantiate the class
 class TextNode():
-    def __init__(self, text, text_type, url=None):
+    def __init__(self, text: str, text_type: TextType, url: str | None = None):
         self.text = text
         self.text_type = text_type
         self.url = url
@@ -24,3 +26,27 @@ class TextNode():
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+def text_node_to_html_node(text_node: TextNode) -> LeafNode:
+    text_type_vals = [text_type.name for text_type in TextType]
+
+    if text_node.text_type.name not in text_type_vals:
+        raise ValueError("TextNode: invalid text type")
+
+    if text_node.text_type is TextType.TEXT:
+        return LeafNode(None, text_node.text)
+    elif text_node.text_type is TextType.BOLD:
+        return LeafNode("b", text_node.text)
+    elif text_node.text_type is TextType.ITALIC:
+        return LeafNode("i", text_node.text)
+    elif text_node.text_type is TextType.CODE:
+        return LeafNode("code", text_node.text)
+    elif text_node.text_type is TextType.LINK:
+        if text_node.url is None:
+            raise ValueError("TextNode: LINK expects a URL")
+        return LeafNode("a", text_node.text, {"href": text_node.url})
+    else:
+        if text_node.url is None or text_node.text is None:
+            raise ValueError("TextNode: IMAGE expects url and text")
+
+        return LeafNode("img", None, {"src": text_node.url, "alt": text_node.text})
