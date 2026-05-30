545746807d3939b416998018e6681aa4acb681d8
Nelson Oliva <nelson.ricardo02@gmail.com>
2026-05-30 17:00:02 -0400

    add unit tests for the different cases for transforming text to leaf

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,5 +1,6 @@
 import unittest
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
+from htmlnode import LeafNode
 
 class TestTextNode(unittest.TestCase):
     # check if equality works correctly
@@ -21,6 +22,45 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is some random text", TextType.ITALIC)
         expect_out = "TextNode(This is some random text, italic, None)"
         self.assertEqual(repr(node), expect_out)
+    def test_textnode_to_html_text(self):
+        node = TextNode("text", TextType.TEXT)
+        rep_node = text_node_to_html_node(node)
+        exp_leaf_node = LeafNode(None, "text")
+        self.assertEqual(repr(exp_leaf_node), repr(rep_node))
+    def test_textnode_to_html_bold(self):
+        node = TextNode("text", TextType.BOLD)
+        rep_node = text_node_to_html_node(node)
+        exp_leaf_node = LeafNode("b", "text")
+        self.assertEqual(repr(exp_leaf_node), repr(rep_node)) 
+    def test_textnode_to_html_italic(self):
+        node = TextNode("text", TextType.ITALIC)
+        rep_node = text_node_to_html_node(node)
+        exp_leaf_node = LeafNode("i", "text")
+        self.assertEqual(repr(exp_leaf_node), repr(rep_node))
+    def test_textnode_to_html_code(self):
+        node = TextNode("text", TextType.CODE)
+        rep_node = text_node_to_html_node(node)
+        exp_leaf_node = LeafNode("code", "text")
+        self.assertEqual(repr(exp_leaf_node), repr(rep_node))
+    def test_textnode_to_html_link(self):
+        node = TextNode("text", TextType.LINK, "example.com")
+        rep_node = text_node_to_html_node(node)
+        exp_leaf_node = LeafNode("a", "text", {"href": "example.com"})
+        self.assertEqual(repr(exp_leaf_node), repr(rep_node))
+    def test_textnode_to_html_image(self):
+        node = TextNode("text", TextType.IMAGE, "example.com")
+        rep_node = text_node_to_html_node(node)
+        exp_leaf_node = LeafNode("img", None, {"src": "example.com", "alt": "text"})
+        self.assertEqual(repr(exp_leaf_node), repr(rep_node))
+    def test_textnode_to_html_link_no_url(self):
+        node = TextNode("text", TextType.LINK, None)
+        with self.assertRaises(ValueError):
+            rep_node = text_node_to_html_node(node)
+    def test_textnode_to_html_image_no_url(self):
+        node = TextNode("text", TextType.IMAGE)
+        with self.assertRaises(ValueError):
+            rep_node = text_node_to_html_node(node)
+    
         
 
 if __name__ == "__main__":
