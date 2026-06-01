ed2474947ee5ab04b128f663a2a18876e077eaec
Nelson Oliva <nelson.ricardo02@gmail.com>
2026-06-01 19:29:33 -0400

    made import corrections for the splitting functions

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 from textnode import TextNode, TextType
-from textnode_functions import text_node_to_html_node, extract_markdown_images, extract_markdown_links, split_nodes_images, split_nodes_link
+from textnode_functions import text_node_to_html_node
+from node_split_func import extract_markdown_images, extract_markdown_links, split_nodes_images, split_nodes_link, text_to_textnodes
 from htmlnode import HTMLNode, LeafNode, ParentNode
 
 def main():
-    image_string = "I have a very cute cat, here is a picture of it: [cute cat](http://example.com) and I love it, I also have a dog, here it is: [cute dog](http://example.com) I love my animals"
-    node = TextNode(image_string, TextType.TEXT)
-    list = split_nodes_link([node, node, node, TextNode("yurr", TextType.BOLD), node])
+    image_string = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+    nodes = text_to_textnodes(image_string)
 
-    print(list)
+    print(nodes)
 
 
 if __name__ == "__main__":
