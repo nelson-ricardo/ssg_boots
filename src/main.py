58a2914d7780f9fe33deb654382d60ba7f20ee43
Nelson Oliva <nelson.ricardo02@gmail.com>
2026-05-31 20:44:46 -0400

    add simple testing

--- src/main.py
+++ src/main.py
@@ -1,9 +1,26 @@
-from textnode import TextNode, TextType, text_node_to_html_node
+from textnode import TextNode, TextType
+from textnode_functions import text_node_to_html_node
 from htmlnode import HTMLNode, LeafNode, ParentNode
 
 def main():
-        random_node = TextNode("text", TextType.BOLD)
-        new_node = text_node_to_html_node(random_node)
+    DELIM = "**"
+
+    first_string: str = "**I** have a small house"
+    second_string: str = "I have a **small** house"
+    third_string: str = "I have a small **house**"
+
+    split_string_first = third_string.split("**")
+    ret_list = list()
+
+    for i, string in enumerate(split_string_first):
+        if string == "":
+            continue
+        if (i + 1) % 2 == 0:
+            ret_list.append(TextNode(string, TextType.BOLD))
+        else:
+            ret_list.append(TextNode(string, TextType.TEXT))
+    
+    print(ret_list)
 
 if __name__ == "__main__":
     main()
