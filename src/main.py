336dcecf60484b3e742ea76001bfe003cf01e0c1
Nelson Oliva <nelson.ricardo02@gmail.com>
2026-06-08 18:09:26 -0400

    add code to build html from markdown

--- src/main.py
+++ src/main.py
@@ -1,22 +1,37 @@
+import os, shutil
+
 from textnode import TextNode, TextType
 from textnode_functions import text_node_to_html_node
-from node_split_func import extract_markdown_images, extract_markdown_links, split_nodes_images, split_nodes_link, text_to_textnodes, markdown_to_blocks
+from node_split_func import extract_markdown_images, extract_markdown_links, split_nodes_images, split_nodes_link, text_to_textnodes, markdown_to_blocks, split_nodes_delimiter
 from htmlnode import HTMLNode, LeafNode, ParentNode
 
+def cp_static_to_public():
+
+    # removes the current contents of public if it already exists
+    if os.path.exists("./public"):
+        print("Removing the public folder")
+        shutil.rmtree("./public")
+        os.mkdir("./public")
+    else:
+        print("Nothing to remove")
+        os.mkdir("./public")
+
+    copy_files_to_public("static", "public")
+
+def copy_files_to_public(src_path: str, dest_path: str):
+    items = os.listdir(f"./{src_path}")
+    for item in items:
+        item_file_path = f"./{src_path}/{item}"
+        if os.path.isfile(item_file_path):
+            shutil.copy2(item_file_path, f"./{dest_path}")
+            print(f"I found a file at {item_file_path}.\nI will copy this to ./{dest_path}/{item}")
+        else:
+            os.mkdir(f"./{dest_path}/{item}")
+            copy_files_to_public(f"{src_path}/{item}", f"{dest_path}/{item}")
+            
+
 def main():
-    test_string = '''
-# This is a heading
-
-This is some paragraph that I want to keep. I want to keep writing
-This is part of the same paragraph
-
-- This is a list
-- This is part of the same list
-
-'''
-    nodes = markdown_to_blocks(test_string)
-
-    print(nodes)
+    cp_static_to_public()
 
 
 if __name__ == "__main__":
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -3,6 +3,7 @@ from textnode import TextNode, TextType
 from textnode_functions import text_node_to_html_node
 from node_split_func import split_nodes_delimiter, extract_markdown_links, extract_markdown_images, split_nodes_images, split_nodes_link, text_to_textnodes, markdown_to_blocks
 from htmlnode import LeafNode
+from md_to_html import markdown_to_html_node
 
 class TestTextNode(unittest.TestCase):
     # check if equality works correctly
@@ -171,7 +172,38 @@ This is the same paragraph on a new line
                     "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
                     "- This is a list\n- with items",
                 ],
-            ) 
+            )
+    def test_paragraphs(self):
+        md = """
+This is **bolded** paragraph
+text in a p
+tag here
+
+This is another paragraph with _italic_ text and `code` here
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+        html,
+        "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+        )
+
+def test_codeblock(self):
+    md = """
+```
+This is text that _should_ remain
+the **same** even with inline stuff
+```
+"""
+
+    node = markdown_to_html_node(md)
+    html = node.to_html()
+    self.assertEqual(
+        html,
+        "<div><pre><code>This is text that _should_ remain\nthe **same** even with inline stuff\n</code></pre></div>",
+    ) 
         
 
 if __name__ == "__main__":
