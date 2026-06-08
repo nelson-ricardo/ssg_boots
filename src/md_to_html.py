feb0228330e15fedbde385125fd6bf739d50e796
Nelson Oliva <nelson.ricardo02@gmail.com>
2026-06-08 19:34:11 -0400

    truly wrapping up

--- src/md_to_html.py
+++ src/md_to_html.py
@@ -15,14 +15,19 @@ def markdown_to_html_node(markdown: str) -> ParentNode:
         children.append(html_node)
     return ParentNode("div", children, None)
 
-def extract_title(markdown: str) -> str:
-    heading: list[str] = re.findall(r"^# (.+)", markdown)
+def generate_pages_recursive(
+    dir_path_content: str, template_path: str, dest_dir_path: str, basepath: str
+) -> None:
+    for filename in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        if os.path.isfile(from_path):
+            dest_path = Path(dest_path).with_suffix(".html")
+            generate_page(from_path, template_path, dest_path, basepath)
+        else:
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
 
-    if len(heading) != 1:
-        raise ValueError("Malformed markdown, needs exactly 1 H1 heading")
-    else:
-        return heading[0].strip()
-    
+
 def generate_page(
     from_path: str, template_path: str, dest_path: str | Path, basepath: str
 ) -> None:
@@ -44,17 +49,19 @@ def generate_page(
     template = template.replace('href="/', 'href="' + basepath)
     template = template.replace('src="/', 'src="' + basepath)
 
-def generate_pages_recursive(
-    dir_path_content: str, template_path: str, dest_dir_path: str, basepath: str
-) -> None:
-    for filename in os.listdir(dir_path_content):
-        from_path = os.path.join(dir_path_content, filename)
-        dest_path = os.path.join(dest_dir_path, filename)
-        if os.path.isfile(from_path):
-            dest_path = Path(dest_path).with_suffix(".html")
-            generate_page(from_path, template_path, dest_path, basepath)
-        else:
-            generate_pages_recursive(from_path, template_path, dest_path, basepath)
+    dest_dir_path = os.path.dirname(dest_path)
+    if dest_dir_path != "":
+        os.makedirs(dest_dir_path, exist_ok=True)
+    to_file = open(dest_path, "w")
+    to_file.write(template)
+
+
+def extract_title(md: str) -> str:
+    lines = md.split("\n")
+    for line in lines:
+        if line.startswith("# "):
+            return line[2:]
+    raise ValueError("no title found")
 
 def block_to_html_node(block: str) -> ParentNode:
     block_type = block_to_block_type(block)
